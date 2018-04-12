31de9993a7eae3e37484089b1bb6244f4532fe36
Anuradha Khambete <anukhambete@gmail.com>
2018-04-12 17:44:51 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   arr_words = string.split
   reverse_array = []
-  arr_words.each do |word|
+  arr_words.collect do |word|
     reverse_array << word.reverse
   end
   reverse_array.join(" ")
